fc97e5106ef2143e13fd9e969099ab83a1d44a25
Ahmed-Yaseen99 <186510963+Ahmed-Yaseen99@users.noreply.github.com>
2026-05-19 17:24:15 +0000

    ⚡ Optimize ArpResolver by replacing ReadToEndAsync and String.Split with line-by-line reading

--- Core/ArpResolver.cs
+++ Core/ArpResolver.cs
@@ -181,7 +181,7 @@ public static class ArpResolver
         {
             var psi = new ProcessStartInfo
             {
-                FileName = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "arp.exe"),
+                FileName = "arp",
                 Arguments = "-a",
                 RedirectStandardOutput = true,
                 UseShellExecute = false,
