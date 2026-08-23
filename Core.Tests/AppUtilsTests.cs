66d8c25297f5c7d3ab1ebc9907952777215e8489
Ahmed-Yaseen99 <186510963+Ahmed-Yaseen99@users.noreply.github.com>
2026-08-23 16:51:29 +0000

    🔒 Fix Command Injection in AppUtils.OpenSafeUrl

---
+++ Core.Tests/AppUtilsTests.cs
@@ -0,0 +1,30 @@
+using System;
+using NodeRadarPro.Core;
+using Xunit;
+
+namespace NodeRadarPro.Core.Tests
+{
+    public class AppUtilsTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("not-a-url")]
+        [InlineData("ftp://example.com")]
+        [InlineData("file:///C:/Windows/System32/calc.exe")]
+        [InlineData("javascript:alert(1)")]
+        public void OpenSafeUrl_InvalidOrNonHttpUrl_DoesNotThrow(string? invalidUrl)
+        {
+            var exception = Record.Exception(() => AppUtils.OpenSafeUrl(invalidUrl!));
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void OpenSafeUrl_ValidHttpUrl_DoesNotThrow()
+        {
+            var exception = Record.Exception(() => AppUtils.OpenSafeUrl("https://example.com"));
+            Assert.Null(exception);
+        }
+    }
+}
