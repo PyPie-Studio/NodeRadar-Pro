48160b797c1bb2bc3eb7bb9d4a49d7f5f3d26225
Ahmed-Yaseen99 <186510963+Ahmed-Yaseen99@users.noreply.github.com>
2026-08-23 16:54:58 +0000

    fix(fingerprinting): correct BER length calculation and parsing in SnmpProbe

--- Core/Fingerprinting/Probes/SnmpProbe.cs
+++ Core/Fingerprinting/Probes/SnmpProbe.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("Core.Tests")]
 
 namespace NodeRadarPro.Core.Fingerprinting.Probes;
 
@@ -49,65 +52,114 @@ public class SnmpProbe : IFingerprintProbe
         return result;
     }
 
-    private byte[] BuildSnmpSysDescrRequest(string community)
+    internal static byte[] BuildSnmpSysDescrRequest(string community)
     {
         var communityBytes = Encoding.ASCII.GetBytes(community);
 
         // OID: 1.3.6.1.2.1.1.1.0 (sysDescr.0)
         byte[] oid = { 0x2b, 0x06, 0x01, 0x02, 0x01, 0x01, 0x01, 0x00 };
 
-        var packet = new List<byte>
-        {
-            0x30, 0x00, // Sequence, length (placeholder)
-            0x02, 0x01, 0x01, // Version 2c
-            0x04, (byte)communityBytes.Length // Community string
-        };
-        packet.AddRange(communityBytes);
+        // Varbind = OID (Tag 0x06, len 8, bytes oid) + Null Value (0x05, 0x00)
+        var varbind = new List<byte>();
+        varbind.Add(0x06);
+        varbind.Add((byte)oid.Length);
+        varbind.AddRange(oid);
+        varbind.AddRange(new byte[] { 0x05, 0x00 });
 
-        // PDU GET Request
-        var pdu = new List<byte>
-        {
-            0xa0, 0x00, // GET Request, length (placeholder)
-            0x02, 0x04, 0x00, 0x00, 0x00, 0x01, // Request ID
-            0x02, 0x01, 0x00, // Error status
-            0x02, 0x01, 0x00, // Error index
-            0x30, 0x00, // Varbind list, length
-            0x30, 0x00, // Varbind, length
-            0x06, (byte)oid.Length // Object identifier
-        };
-        pdu.AddRange(oid);
-        pdu.AddRange(new byte[] { 0x05, 0x00 }); // Null value
+        // Varbind List = Sequence (0x30) + Length + Varbind bytes
+        var varbindList = EncodeBerSequence(varbind.ToArray());
 
-        // Fix lengths
-        pdu[17] = (byte)(pdu.Count - 19); // Varbind length
-        pdu[15] = (byte)(pdu.Count - 17); // Varbind list length
-        pdu[1] = (byte)(pdu.Count - 2);   // PDU length
+        // PDU Payload = Request ID (Integer) + Error Status (Integer) + Error Index (Integer) + Varbind List
+        var pduPayload = new List<byte>();
+        pduPayload.AddRange(new byte[] { 0x02, 0x04, 0x00, 0x00, 0x00, 0x01 }); // Request ID
+        pduPayload.AddRange(new byte[] { 0x02, 0x01, 0x00 }); // Error Status
+        pduPayload.AddRange(new byte[] { 0x02, 0x01, 0x00 }); // Error Index
+        pduPayload.AddRange(varbindList);
 
-        packet.AddRange(pdu);
-        packet[1] = (byte)(packet.Count - 2); // Sequence length
+        // PDU (GetRequest 0xa0) + Length + pduPayload
+        var pdu = EncodeBerHeader(0xa0, pduPayload.ToArray());
 
-        return packet.ToArray();
+        // Message Payload = Version (Integer v2c = 1) + Community String (OctetString) + PDU
+        var messagePayload = new List<byte>();
+        messagePayload.AddRange(new byte[] { 0x02, 0x01, 0x01 }); // Version v2c (value 1)
+        messagePayload.Add(0x04); // Octet String
+        messagePayload.Add((byte)communityBytes.Length);
+        messagePayload.AddRange(communityBytes);
+        messagePayload.AddRange(pdu);
+
+        // Outer Message Sequence (0x30) + Length + messagePayload
+        return EncodeBerSequence(messagePayload.ToArray());
     }
 
-    private string ParseSnmpResponse(byte[] buffer)
+    private static byte[] EncodeBerSequence(byte[] content)
+    {
+        return EncodeBerHeader(0x30, content);
+    }
+
+    private static byte[] EncodeBerHeader(byte tag, byte[] content)
+    {
+        var result = new List<byte> { tag };
+        int len = content.Length;
+        if (len < 128)
+        {
+            result.Add((byte)len);
+        }
+        else if (len <= 255)
+        {
+            result.Add(0x81);
+            result.Add((byte)len);
+        }
+        else
+        {
+            result.Add(0x82);
+            result.Add((byte)(len >> 8));
+            result.Add((byte)(len & 0xff));
+        }
+        result.AddRange(content);
+        return result.ToArray();
+    }
+
+    internal static string ParseSnmpResponse(byte[] buffer)
     {
         try
         {
-            // Simple BER parser looking for the last octet string (usually the sysDescr value)
+            // Simple BER parser looking for string values (Octet String 0x04)
             string sysDescr = "";
-            for (int i = 0; i < buffer.Length - 1; i++)
+            int i = 0;
+            while (i < buffer.Length)
             {
                 if (buffer[i] == 0x04) // Octet string
                 {
+                    if (i + 1 >= buffer.Length) break;
                     int len = buffer[i + 1];
+                    int headerLen = 2;
 
-                    if (len >= 128)
+                    if (len > 128)
                     {
-                        string str = Encoding.ASCII.GetString(buffer, i + 2, len);
+                        int numOctets = len & 0x7f;
+                        if (i + 1 + numOctets >= buffer.Length) break;
+                        len = 0;
+                        for (int j = 0; j < numOctets; j++)
+                        {
+                            len = (len << 8) | buffer[i + 2 + j];
+                        }
+                        headerLen = 2 + numOctets;
+                    }
+                    else if (len == 128)
+                    {
+                        i++;
+                        continue;
+                    }
+
+                    if (i + headerLen + len <= buffer.Length && len > 0)
+                    {
+                        string str = Encoding.ASCII.GetString(buffer, i + headerLen, len);
                         if (str.Length > sysDescr.Length) sysDescr = str;
-                        i += len + 1;
+                        i += headerLen + len;
+                        continue;
                     }
                 }
+                i++;
             }
 
             // Clean non-printable characters
